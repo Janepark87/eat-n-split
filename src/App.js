8e7fca3da76922f996b326c368b1041a6aacd524
Jpark <web.jpark@0gmail.com>
2023-11-02 21:27:52 -0600

    display the new friend form while toggling button

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import FriendsList from './FriendsList.jsx';
 import FormAddFriend from './components/FormAddFriend.jsx';
 import Button from './components/Button.jsx';
 import FormSplitBill from './components/FormSplitBill.jsx';
+import { useState } from 'react';
 
 export default function App() {
+	const [showAddFriend, setShowAddFriend] = useState(false);
+
 	return (
 		<div className="app">
 			<aside className="sidebar">
 				<FriendsList />
-				<FormAddFriend />
-				<Button>Add friend</Button>
+
+				{showAddFriend && <FormAddFriend />}
+
+				<Button onClick={() => setShowAddFriend((show) => !show)}>{showAddFriend ? 'Close' : 'Add friend'}</Button>
 			</aside>
 
 			<FormSplitBill />
